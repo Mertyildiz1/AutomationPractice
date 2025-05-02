986fab11a20ea06027589ba518b4933b4e05ef14
Mert <1yildizmert@gmail.com>
2025-05-02 18:42:45 +0300

    Exercise 1 eklendi.

---
+++ src/test/java/Exercises/Exercises.java
@@ -0,0 +1,102 @@
+package Exercises;
+
+import com.github.javafaker.Faker;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Exercises {
+
+    @Test
+    public void test01() {
+        Faker faker = new Faker();
+
+        // Navigate to url 'http://automationexercise.com'
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        String url = "http://automationexercise.com";
+        driver.get(url);
+
+        // Verify that home page is visible successfully
+        String pageExpectedTitle = "Automation Exercise";
+        String pageActualTitle = driver.getTitle();
+        Assert.assertEquals(pageExpectedTitle,pageActualTitle);
+
+        WebElement homePageTestCasesButton = driver.findElement(By.xpath("//*[.=' Test Cases']"));
+        Assert.assertTrue(homePageTestCasesButton.isDisplayed());
+
+        // Click on 'Signup / Login' button
+        WebElement signUpLoginButton = driver.findElement(By.xpath("//i[@class='fa fa-lock']"));
+        signUpLoginButton.click();
+
+        //Verify 'New User Signup!' is visible
+
+        String expectedLoginTitle = "Automation Exercise - Signup / Login";
+        String actualLoginTitle = driver.getTitle();
+        Assert.assertEquals(expectedLoginTitle,actualLoginTitle);
+
+        //Enter name and email address
+        WebElement nameArea = driver.findElement(By.xpath("//input[@name='name']"));
+        nameArea.sendKeys(faker.name().firstName(),
+                Keys.TAB,faker.internet().emailAddress());
+
+        //Click 'Signup' button
+        WebElement signUp = driver.findElement(By.xpath("//button[.='Signup']"));
+        signUp.click();
+
+        //Verify that 'ENTER ACCOUNT INFORMATION' is visible
+        WebElement entAccInfoText = driver.findElement(By.xpath("//*[.='Enter Account Information']"));
+        Assert.assertTrue(entAccInfoText.isDisplayed());
+
+        //Fill details: Title, Name, Email, Password, Date of birth
+        WebElement mrsMrSelect = driver.findElement(By.id("id_gender1"));
+        mrsMrSelect.click();
+        mrsMrSelect.sendKeys(Keys.TAB,Keys.TAB,faker.internet().password(),Keys.TAB,"30",Keys.TAB,"July",Keys.TAB,"2001");
+
+        //Select checkbox 'Sign up for our newsletter!'
+        WebElement sufons = driver.findElement(By.id("newsletter"));
+        sufons.click();
+        sufons.sendKeys(Keys.TAB,Keys.SPACE);
+
+        // Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
+        WebElement firstName = driver.findElement(By.id("first_name"));
+        firstName.sendKeys(faker.name().firstName(),Keys.TAB,faker.name().lastName(),Keys.TAB,faker.company().name(),Keys.TAB,faker.address().fullAddress(),Keys.TAB,
+                Keys.TAB,faker.address().country(),Keys.TAB,faker.address().state(),Keys.TAB,faker.address().city(),Keys.TAB,faker.address().zipCode());
+        WebElement mobileNumberInput = driver.findElement(By.id("mobile_number"));
+        mobileNumberInput.sendKeys(faker.phoneNumber().phoneNumber());
+
+        //Click 'Create Account button'
+        WebElement createAccButton = driver.findElement(By.xpath("//*[.='Create Account']"));
+        createAccButton.click();
+
+        // Verify that 'ACCOUNT CREATED!' is visible
+        WebElement accCreated = driver.findElement(By.xpath("//*[.='Account Created!']"));
+        Assert.assertTrue(accCreated.isDisplayed());
+        Assert.assertEquals("Automation Exercise - Account Created",driver.getTitle());
+
+        // Click 'Continue' button
+        WebElement continueButton = driver.findElement(By.xpath("//*[.='Continue']"));
+        continueButton.click();
+
+        //Verify that 'Logged in as username' is visible
+        WebElement loggedInAsUser = driver.findElement(By.xpath("//*[text()=' Logged in as ']"));
+        Assert.assertTrue(loggedInAsUser.isDisplayed());
+
+        //Click 'Delete Account' button
+        WebElement deleteAccButton = driver.findElement(By.xpath("//*[.=' Delete Account']"));
+        deleteAccButton.click();
+
+        // Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
+        WebElement accDeletedText = driver.findElement(By.xpath("//*[.='Account Deleted!']"));
+        Assert.assertTrue(accDeletedText.isDisplayed());
+
+    }
+}
---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,70 @@
+package utilities;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+public class TestBase {
+
+    /*
+        Test Class içersinde her defasında driver ayarları yapmak yerine
+     driver ayarlarını TestBase ismini verdiğimiz class'da tutabiliriz.
+     Büyük projelerde method ve class isimlerinde karışıklıklar olabilir
+     bunun önüne geçmek için bu class'ı abstract yaparız ve obje oluşumunun
+     önüne geçmiş oluruz. Böylece bu class'î sadece miras alarak kullanabiliriz.
+     */
+    protected WebDriver driver;
+    protected Select select;
+    protected Actions actions;
+    @Before
+    public void setUp() throws Exception {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+        /*
+            ImplicitlyWait seleniumdan gelen akıllı/dinamik bekleme türlerinden biridir.
+        Nasıl manage() methodu ile tarayıcıya maximum olma özelliğini veriyorsak aynı manage()
+        methodu ile tarayıcıya max. 15 saniye(Duration.ofSeconds(15)) elemenler oluşana kadar bekle demiş oluyoruz.
+            ImplicitlyWait bu beklemeyi tüm elementler için uygular taki oturum sonlanana kadar(driver.quit()).
+        Element 2 saniyede oluşursa bir alt satıra geçer ama element belirtilen max. süre boyunca oluşmaz/görüntünemez
+        ise exception fırlatır kod çalışmayı durdurur.
+         */
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+    //Hard Wait
+    public void bekle(int saniye){
+        try {
+            Thread.sleep(1000*saniye);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Pencere Geçiş
+    public void window(int index){
+        driver.switchTo().window(driver.getWindowHandles().toArray()[index].toString());
+    }
+
+    //DDM index
+    public void selectIndex(WebElement ddmElement, int index){
+        select = new Select(ddmElement);
+        select.selectByIndex(index);
+    }
+
+    //Alert Accept
+    public void alertOK(){
+        driver.switchTo().alert().accept();
+    }
+}
