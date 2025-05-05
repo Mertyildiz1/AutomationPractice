efa96b1c648fa598448c9498a0eb6cdf82a5ec74
Mert <1yildizmert@gmail.com>
2025-05-05 17:41:57 +0300

    Exercise 3 eklendi.

---
+++ src/test/java/Exercises/LoginWithIncorrectEmailAndPassword.java
@@ -0,0 +1,54 @@
+package Exercises;
+
+import com.github.javafaker.Faker;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utilities.TestBase;
+
+import java.time.Duration;
+
+public class LoginWithIncorrectEmailAndPassword extends TestBase {
+    @Test
+    public void test03(){
+        Faker faker = new Faker();
+        driver.get("http://automationexercise.com");
+
+        //Verify that home page is visible successfully
+        WebElement featuresItemsText = driver.findElement(By.xpath("//h2[@class='title text-center']"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.visibilityOf(featuresItemsText));
+        Assert.assertTrue(featuresItemsText.isDisplayed());
+
+        String expectedTitle = "Automation Exercise";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(expectedTitle,actualTitle);
+
+        // Click on 'Signup / Login' button
+
+        WebElement signUpLoginButton = driver.findElement(By.xpath("//*[text()=' Signup / Login']"));
+        signUpLoginButton.click();
+
+        //Verify 'Login to your account' is visible
+        WebElement loginToYourAccText = driver.findElement(By.xpath("//*[text()='Login to your account']"));
+        wait.until(ExpectedConditions.visibilityOf(loginToYourAccText));
+        Assert.assertTrue(loginToYourAccText.isDisplayed());
+
+        //Enter incorrect email address and password
+        WebElement emailArea = driver.findElement(By.xpath("//*[@type='email']"));
+        emailArea.sendKeys(faker.internet().emailAddress(), Keys.TAB,faker.internet().password(),Keys.TAB,Keys.ENTER);
+
+        //Verify error 'Your email or password is incorrect!' is visible
+        WebElement errorText = driver.findElement(By.xpath("//*[text()='Your email or password is incorrect!']"));
+        String expectedErrorText = "Your email or password is incorrect!";
+
+        Assert.assertTrue(errorText.isDisplayed());
+        Assert.assertEquals(expectedErrorText,errorText.getText());
+
+
+    }
+}
--- src/test/java/Exercises/RegisterUser.java
+++ src/test/java/Exercises/RegisterUser.java
@@ -8,12 +8,10 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class Exercises {
+public class RegisterUser {
 
     @Test
     public void RegisterUser() {
@@ -99,55 +97,5 @@ public class Exercises {
         // Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
         WebElement accDeletedText = driver.findElement(By.xpath("//*[.='Account Deleted!']"));
         Assert.assertTrue(accDeletedText.isDisplayed());
-
-    }
-
-    @Test
-    public void LoginWithCorrectEmailAndPassword() {
-        String url = "http://automationexercise.com";
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-
-        // Navigate to url 'http://automationexercise.com'
-        driver.get(url);
-
-        //Verify that home page is visible successfully
-        WebElement featuresItemsText = driver.findElement(By.xpath("//h2[@class='title text-center']"));
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(15));
-        wait.until(ExpectedConditions.visibilityOf(featuresItemsText));
-        Assert.assertTrue(featuresItemsText.isDisplayed());
-
-        String expectedTitle = "Automation Exercise";
-        String actualTitle = driver.getTitle();
-        Assert.assertEquals(expectedTitle,actualTitle);
-
-        // Click on 'Signup / Login' button
-
-        WebElement signUpLoginButton = driver.findElement(By.xpath("//*[text()=' Signup / Login']"));
-        signUpLoginButton.click();
-
-        //Verify 'Login to your account' is visible
-        WebElement loginToYourAccText = driver.findElement(By.xpath("//*[text()='Login to your account']"));
-        wait.until(ExpectedConditions.visibilityOf(loginToYourAccText));
-        Assert.assertTrue(loginToYourAccText.isDisplayed());
-
-        //Enter correct email address and password
-        WebElement emailArea = driver.findElement(By.xpath("//*[@type='email']"));
-        emailArea.sendKeys("1yildizmert@gmail.com",Keys.TAB,"test123456",Keys.TAB,Keys.ENTER);
-
-        //Verify that 'Logged in as username' is visible
-        WebElement loggedInAs = driver.findElement(By.xpath("//*[text()=' Logged in as ']"));
-        wait.until(ExpectedConditions.visibilityOf(loggedInAs));
-        Assert.assertTrue(loggedInAs.isDisplayed());
-
-        //Click 'Delete Account' button
-        WebElement deleteAccButton = driver.findElement(By.xpath("//*[.=' Delete Account']"));
-        deleteAccButton.click();
-
-        // Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
-        WebElement accDeletedText = driver.findElement(By.xpath("//*[.='Account Deleted!']"));
-        Assert.assertTrue(accDeletedText.isDisplayed());
-
     }
 }
