75a27a4cd8e69c838e6ffe0bffd1a362e517eb91
Mert <1yildizmert@gmail.com>
2025-05-05 17:17:08 +0300

    Exercise 2 eklendi.

--- src/test/java/Exercises/Exercises.java
+++ src/test/java/Exercises/Exercises.java
@@ -8,13 +8,15 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class Exercises {
 
     @Test
-    public void test01() {
+    public void RegisterUser() {
         Faker faker = new Faker();
 
         // Navigate to url 'http://automationexercise.com'
@@ -89,6 +91,55 @@ public class Exercises {
         //Verify that 'Logged in as username' is visible
         WebElement loggedInAsUser = driver.findElement(By.xpath("//*[text()=' Logged in as ']"));
         Assert.assertTrue(loggedInAsUser.isDisplayed());
+
+        //Click 'Delete Account' button
+        WebElement deleteAccButton = driver.findElement(By.xpath("//*[.=' Delete Account']"));
+        deleteAccButton.click();
+
+        // Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
+        WebElement accDeletedText = driver.findElement(By.xpath("//*[.='Account Deleted!']"));
+        Assert.assertTrue(accDeletedText.isDisplayed());
+
+    }
+
+    @Test
+    public void LoginWithCorrectEmailAndPassword() {
+        String url = "http://automationexercise.com";
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        // Navigate to url 'http://automationexercise.com'
+        driver.get(url);
+
+        //Verify that home page is visible successfully
+        WebElement featuresItemsText = driver.findElement(By.xpath("//h2[@class='title text-center']"));
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.visibilityOf(featuresItemsText));
+        Assert.assertTrue(featuresItemsText.isDisplayed());
+
+        String expectedTitle = "Automation Exercise";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(expectedTitle,actualTitle);
+
+        // Click on 'Signup / Login' button
+
+        WebElement signUpLoginButton = driver.findElement(By.xpath("//*[text()=' Signup / Login']"));
+        signUpLoginButton.click();
+
+        //Verify 'Login to your account' is visible
+        WebElement loginToYourAccText = driver.findElement(By.xpath("//*[text()='Login to your account']"));
+        wait.until(ExpectedConditions.visibilityOf(loginToYourAccText));
+        Assert.assertTrue(loginToYourAccText.isDisplayed());
+
+        //Enter correct email address and password
+        WebElement emailArea = driver.findElement(By.xpath("//*[@type='email']"));
+        emailArea.sendKeys("1yildizmert@gmail.com",Keys.TAB,"test123456",Keys.TAB,Keys.ENTER);
+
+        //Verify that 'Logged in as username' is visible
+        WebElement loggedInAs = driver.findElement(By.xpath("//*[text()=' Logged in as ']"));
+        wait.until(ExpectedConditions.visibilityOf(loggedInAs));
+        Assert.assertTrue(loggedInAs.isDisplayed());
 
         //Click 'Delete Account' button
         WebElement deleteAccButton = driver.findElement(By.xpath("//*[.=' Delete Account']"));
